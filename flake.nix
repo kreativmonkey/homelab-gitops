5c5fbb4cb41fa6b93867db87597c19eb73444eec
Sebastian Preisner <preisner@puzzle-itc.de>
2026-03-13 16:41:10 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Ansible Schulung Entwicklungsumgebung";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    age =
+      pkgs.mkShell
+        {
+        # Required packages
+        buildInputs = [
+          pkgs.age
+        ];
+
+        # Shell hook replicates Dockerfile steps
+        shellHook = ''
+          cd dns
+        '';
+        };
+  };
+}
